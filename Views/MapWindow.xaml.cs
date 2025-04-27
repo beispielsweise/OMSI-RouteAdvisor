0be8417667137f29b8a0c2b8d6fbc2f824b3cc73
beispielsweise <egputilin@gmail.com>
2025-04-27 13:06:11 +0200

    MapWindow changes * added Buttons and sliders for settings * changed visuals * added class to manage Topmost states of the window (for fullscreen applications) * Center Map parameter tested

--- Views/MapWindow.xaml.cs
+++ Views/MapWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Brushes = System.Windows.Media.Brushes;
 using Brush = System.Windows.Media.Brush;
+using OMSI_RouteAdvisor.Views.Misc;
 
 namespace OMSI_RouteAdvisor.Views
 {
@@ -24,23 +25,24 @@ namespace OMSI_RouteAdvisor.Views
     {
         public MapData MapData { get; set; }
         private TranslateTransform MoveTransform { get; set; }
+        private ScaleTransform ScaleTransform { get; set; }
         private Brush Active { get; set; }
         private Brush Passive { get; set; }
         private Dictionary<int, Ellipse> BusStopPositions { get; set; }
+        private bool _isWindowFixed = false;
 
         public MapWindow(string mapFolderPath)
         {
             InitializeComponent();
 
             MapData = new(mapFolderPath);
-            MoveTransform = new(); 
             Active = Brushes.Green;
             Passive = Brushes.Red;
             BusStopPositions = new Dictionary<int, Ellipse>();
 
             LoadMapBmp();
             DrawBusStops();
-
+            
             // Empty for now
             SetupMapMovement();
         }
@@ -82,25 +84,87 @@ namespace OMSI_RouteAdvisor.Views
             }
         }
 
-        //
+        /// <summary>
+        /// Initialises Map movement variables
+        /// </summary>
         private void SetupMapMovement()
         {
-            // Set up the TranslateTransform for panning
-            MapCanvas.RenderTransform = MoveTransform;
+            ScaleTransform = new ScaleTransform(1.0, 1.0);
+            MoveTransform = new TranslateTransform();
+
+            TransformGroup group = new TransformGroup();
+            group.Children.Add(ScaleTransform);
+            group.Children.Add(MoveTransform);
+
+            MapCanvas.RenderTransform = group;
         }
 
-        //
-        public void CenterMapOnBus(double busPixelX, double busPixelY, double viewportWidth, double viewportHeight)
+        /// <summary>
+        /// Changes the zoom of the map with a slider
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MapZoomSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            // Update map position to center bus
-            MoveTransform.X = -busPixelX + (viewportWidth / 2);
-            MoveTransform.Y = -busPixelY + (viewportHeight / 2);
+            if (ScaleTransform != null)
+            {
+                ScaleTransform.ScaleX = MapZoomSlider.Value;
+                ScaleTransform.ScaleY = MapZoomSlider.Value;
+            }
         }
 
-        // Move window on click and hold
+        /// <summary>
+        /// Changes the window to be unresizible unmovable and topmost with a checkbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FixWindowPositionButton_Click(object sender, RoutedEventArgs e)
+        {
+            _isWindowFixed = !_isWindowFixed;
+
+            if (_isWindowFixed)
+            {
+                this.ResizeMode = ResizeMode.NoResize;
+                ChangeWindowZ.MakeTopmost(this);
+            } else
+            {
+                this.ResizeMode= ResizeMode.CanResize;
+                ChangeWindowZ.RevertTopmost(this);
+            }
+        }
+
+        /// <summary>
+        /// Closes the Map and returns to the <c>MainWindow</c>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CloseMapButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        /// <summary>
+        /// CentersMap on a given coordinate
+        /// </summary>
+        private void CenterMap(double x, double y)
+        {
+            double centerX = (MapBackground.Width * ScaleTransform.ScaleX) / 2.0;
+            double centerY = (MapBackground.Height * ScaleTransform.ScaleY) / 2.0;
+
+            MoveTransform.X = -(centerX - (this.ActualWidth / 2.0));
+            MoveTransform.Y = -(centerY - ((this.ActualHeight - FixWindowPositionButton.ActualHeight) / 2.0));
+        }
+
+        /// <summary>
+        /// Allows the window to be moved on a click and hold action
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void RootGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.ButtonState == MouseButtonState.Pressed)
+            if (e.ButtonState == MouseButtonState.Pressed && !_isWindowFixed)
                 this.DragMove();
         }
     }
---
+++ Views/Misc/ChangeWindowZ.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OMSI_RouteAdvisor.Views.Misc
+{
+    /// <summary>
+    /// Changes internal Z position of a window
+    /// </summary>
+    internal class ChangeWindowZ
+    {
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_NOMOVE = 0x0002;
+        private const uint TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+        /// <summary>
+        /// Makes window Topmost over any fullscreen application
+        /// </summary>
+        /// <param name="mapWindow"></param>
+        public static void MakeTopmost(Window window)
+        {
+            var handle = new System.Windows.Interop.WindowInteropHelper(window).Handle;
+            SetWindowPos(handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+        }
+
+        /// <summary>
+        /// Reverts window Topmost parameter back to normal
+        /// </summary>
+        /// <param name="mapWindow"></param>
+        public static void RevertTopmost(Window window)
+        {
+            var handle = new System.Windows.Interop.WindowInteropHelper(window).Handle;
+            SetWindowPos(handle, HWND_NOTOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+        }
+    }
+}
